c12cab69b9ddaafa769d383a15156c0e93583016
yyymmmccc <jimindong9814@gmail.com>
2025-01-17 05:48:43 +0900

    "회원가입 기능"

--- src/main/java/com/example/food/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.food.service;
-
-import com.example.food.dto.request.auth.SignUpRequestDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-public interface AuthService {
-    ResponseEntity registerUser(SignUpRequestDto dto);
-}
